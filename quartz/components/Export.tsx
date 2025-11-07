4a3cf8d3d8e6f8d2113c41f88f2fd73e337812d3
Krish Matta <self@krishmatta.net>
2025-11-07 15:47:47 -0500

    Remove pdf exports

--- quartz/components/Export.tsx
+++ quartz/components/Export.tsx
@@ -1,4 +1,4 @@
-import { FullSlug, joinSegments, pathToRoot } from "../util/path"
+import { FilePath, FullSlug, joinSegments, pathToRoot } from "../util/path"
 import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 type ExportData = {
@@ -18,22 +18,40 @@ export default ((opts?: Partial<ExportOptions>) => {
   // Merge options with defaults
   const options: ExportOptions = { ...defaultOptions, ...opts }
 
-  function Export({ cfg, fileData }: QuartzComponentProps) {
-    const exports: ExportData[] = options.fileExports.map(fileExport => {
-	const baseDir = joinSegments("..", pathToRoot(fileData.slug))
-	const exportDir = joinSegments(baseDir, fileExport)
-	const exportPath = joinSegments(exportDir, `${fileData.slug}.${fileExport}`)
-	return { displayName: fileExport, path: exportPath }
+  function Export({ ctx, fileData }: QuartzComponentProps) {
+    const slug = fileData.slug as FullSlug | undefined
+    if (!slug) {
+      return null
+    }
+
+    const availableFiles = new Set<FilePath>(ctx.allFiles)
+
+    const exports: ExportData[] = options.fileExports.flatMap((fileExport) => {
+      const relativeExportFile = joinSegments(fileExport, `${slug}.${fileExport}`)
+
+      if (!availableFiles.has(relativeExportFile as FilePath)) {
+        return []
+      }
+
+      const baseDir = joinSegments("..", pathToRoot(slug))
+      const exportDir = joinSegments(baseDir, fileExport)
+      const exportPath = joinSegments(exportDir, `${slug}.${fileExport}`)
+
+      return [{ displayName: fileExport, path: exportPath }]
     })
+
+    if (exports.length === 0) {
+      return null
+    }
 
     return (
       <div class="export">
         {exports.map(({ displayName, path }, index) => (
-	  <span>
-  	    <a href={path}>{displayName}</a>
-	    {index < exports.length - 1 ? " | " : ""}
-	  </span>
-	))}
+          <span>
+            <a href={path}>{displayName}</a>
+            {index < exports.length - 1 ? " | " : ""}
+          </span>
+        ))}
       </div>
     )
   }
